196df6a65053c57a44170553e626675132f61ce4
Iida Autio <iida.autio@gmail.com>
2026-05-01 22:57:54 +0300

    Add new classes

--- init.sql
+++ init.sql
@@ -3,6 +3,7 @@ DELETE FROM classes;
 INSERT INTO classes (title, value) VALUES ('Tekeminen', 'Leikit kaverin kotona');
 INSERT INTO classes (title, value) VALUES ('Tekeminen', 'Leikit ulkona leikkipuistossa');
 INSERT INTO classes (title, value) VALUES ('Tekeminen', 'Sisäleikkipuisto');
+INSERT INTO classes (title, value) VALUES ('Tekeminen', 'Kirjasto');
 INSERT INTO classes (title, value) VALUES ('Tekeminen', 'Urheilu');
 INSERT INTO classes (title, value) VALUES ('Tekeminen', 'Uimaranta');
 INSERT INTO classes (title, value) VALUES ('Tekeminen', 'Muu');
